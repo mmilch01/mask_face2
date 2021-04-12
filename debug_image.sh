8d554353c8167d111cbde6a64d815ef63d4a8408
Mikhail Milchenko <mmilchenko@wustl.edu>
2021-04-12 14:11:48 -0500

    +

--- debug_image.sh
+++ debug_image.sh
@@ -5,6 +5,6 @@ if [ -z "$1" ]; then
 	exit -1
 fi
 
-echo docker run -u $(id -u ${USER}):$(id -g ${USER}) -v `pwd`:/docker_mount -it --rm --gpus all registry.nrg.wustl.edu/docker/nrg-repo/facemasking2:latest mask_face_nomatlab $@
-docker run -u $(id -u ${USER}):$(id -g ${USER}) -v `pwd`:/docker_mount -it --rm --gpus all registry.nrg.wustl.edu/docker/nrg-repo/facemasking2:latest mask_face_nomatlab $@
+echo docker run --network="host" -u $(id -u ${USER}):$(id -g ${USER}) -v `pwd`:/docker_mount -it --rm --gpus all registry.nrg.wustl.edu/docker/nrg-repo/facemasking2:latest mask_face_nomatlab $@
+docker run --network="host" -u $(id -u ${USER}):$(id -g ${USER}) -v `pwd`:/docker_mount -it --rm --gpus all registry.nrg.wustl.edu/docker/nrg-repo/facemasking2:latest mask_face_nomatlab $@
 
